978f12b40d4ed6153c314e591a21f8554a466120
Polly <polli.burjan@gmail.com>
2020-11-25 15:12:08 +0100

    Hello

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask('codecool_series')
 
+
 @app.route('/')
 def index():
     shows = queries.get_shows()
@@ -23,3 +24,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# Hello
