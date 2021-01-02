a70ea1224a2c0dea844cec50eaaa5ac16185a112
Polly <polli.burjan@gmail.com>
2021-01-02 22:59:25 +0100

    Add PA practices

--- data/queries.py
+++ data/queries.py
@@ -3,3 +3,45 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+
+def entry_level_1():
+    return data_manager.execute_select("""SELECT shows.title, COUNT(e.id) AS episodes
+                                            FROM shows
+                                            JOIN seasons ON seasons.show_id = shows.id
+                                            JOIN episodes e ON seasons.id = e.season_id
+                                            GROUP BY shows.title
+                                            ORDER BY shows.title ASC;""")
+
+
+def entry_level_2():
+    return data_manager.execute_select(""" SELECT actors.name, COUNT(DISTINCT sc.character_name) AS counts, 
+                                            string_agg(DISTINCT sc.character_name::text, ', ')
+                                            FROM actors
+                                            JOIN show_characters sc ON actors.id = sc.actor_id
+                                            GROUP BY actors.name
+                                            ORDER BY counts DESC LIMIT 10;""")
+
+
+def entry_level_3(season_num, episode_num):
+    return data_manager.execute_select("""SELECT shows.title AS shows
+                                            FROM shows
+                                            JOIN seasons s ON shows.id = s.show_id
+                                            JOIN episodes e ON s.id = e.season_id
+                                            JOIN show_genres sg ON shows.id = sg.show_id
+                                            JOIN genres g ON g.id = sg.genre_id
+                                            GROUP BY shows.title
+                                            HAVING COUNT( DISTINCT s.season_number) >= %(season_num)s
+                                            AND COUNT(e.episode_number) >= %(episode_num)s;""",
+                                       {'season_num': season_num, 'episode_num': episode_num})
+
+
+def pa_1(genre):
+    return data_manager.execute_select("""SELECT shows.title, shows.year, ROUND(shows.rating)::INTEGER AS rating
+                                            FROM shows
+                                            JOIN show_genres sg ON shows.id = sg.show_id
+                                            JOIN genres g ON g.id = sg.genre_id
+                                            WHERE g.name = %(genre)s
+                                            GROUP BY shows.title, shows.year, shows.rating
+                                            ORDER BY shows.rating DESC LIMIT 10;""",
+                                       {'genre': genre})
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from data import queries
 import math
 from dotenv import load_dotenv
@@ -16,6 +16,50 @@ def index():
 @app.route('/design')
 def design():
     return render_template('design.html')
+
+
+@app.route('/entry_level_1')
+def entry_level_1():
+    episodes = queries.entry_level_1()
+    for episode in episodes:
+        if episode['episodes'] > 99:
+            episode['is_long'] = True
+        else:
+            episode['is_long'] = False
+    return render_template('entry_level_1.html', episodes=episodes)
+
+
+@app.route('/entry_level_2')
+def entry_level_2():
+    data = queries.entry_level_2()
+    data[0]['name'] = data[0]['name'] + " 🥇"
+    data[1]['name'] = data[1]['name'] + " 🥈"
+    data[2]['name'] = data[2]['name'] + " 🥉"
+    all_count = 0
+    for person in data:
+        all_count = all_count + person['counts']
+    return render_template('entry_level_2.html', data=data, roles=all_count)
+
+
+@app.route('/entry_level_3', methods=['GET', 'POST'])
+def entry_level_3():
+    if request.method == 'POST':
+        data = ""
+        season = request.form['season']
+        episode = request.form['episode']
+        print(season, episode)
+        data = queries.entry_level_3(season, episode)
+        return render_template('entry_level_3.html', data=data)
+    return render_template('entry_level_3.html', data=None)
+
+
+@app.route('/pa_1', methods=['GET', 'POST'])
+def pa_1():
+    if request.method == 'POST':
+        genre = request.form['genre']
+        data = queries.pa_1(genre)
+        return render_template('pa_1.html', data=data)
+    return render_template('pa_1.html', data=None)
 
 
 def main():
